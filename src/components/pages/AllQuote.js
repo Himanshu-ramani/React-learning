5472660fd63ebf0a724d57f2f0c976b2cbb4e36d
Himanshu Ramani <ramanihimanshu58@gmail.com>
2022-03-15 11:02:28 +0530

    work with routing and route and nested route to

---
+++ src/components/pages/AllQuote.js
@@ -0,0 +1,35 @@
+import React,{useEffect} from "react";
+import QuoteList from "../quotes/QuoteList"
+// import NewQuote from "./NewQuote";
+
+import useHttp from "../hooks/use-http";
+import {getAllQuotes} from '../lib/api'
+import LoadingSpinner from "../UI/LoadingSpinner";
+import NoQuotesFound from "../quotes/NoQuotesFound";
+const DUMMY_QUOTES  = [
+    {id:"q1" , author:"himanshu" , text:"react having fun!"},
+    {id:"q2" , author:"himanshuRamani" , text:"react make great website!"},
+]
+const AllQuote = () => {
+ const {sendRequest ,status , data: loadedQuotes , error} = useHttp(getAllQuotes,true)
+ useEffect(() => {
+  sendRequest()
+ }, [sendRequest])
+ if (status === "pending") {
+   return (
+     <div className="centered">
+       <LoadingSpinner />
+     </div>
+   )
+ }
+ if (error) {
+   return <p className="centered"> {error}</p>
+ }
+ if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+  return <NoQuotesFound />   
+}else{
+  return <QuoteList quotes={DUMMY_QUOTES} />
+}
+};
+
+export default AllQuote;
